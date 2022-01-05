4feb572830fed0b0d31160dc4f50d6cd6cff5cf6
ryutaro-kimura <sasuker2264@gmail.com>
2022-01-05 22:39:35 +0900

    update: step2のAPIストレージの追加

--- server/src/main.js
+++ server/src/main.js
@@ -1,3 +1,23 @@
+// データはサーバーではなく「ローカルストレージ」へ保存することにする
+// https://jp.vuejs.org/v2/examples/todomvc.html
+// 一種のライブラリと思えば良い
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
 const app = new Vue({
     el: '#app',
     data: {
